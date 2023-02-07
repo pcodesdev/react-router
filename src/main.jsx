98c4bf07b4a303c281f55af3d4b507054f21ee75
Peter Njuguna <peter.njuguna@kirkonulkomaanapu.fi>
2023-02-07 15:16:13 +0300

    added error handling capabilities

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,12 @@ import {
 } from "react-router-dom";
 import './index.css'
 import Root from './routes/root';
+import ErrorPage from './error-page';
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    errorElement: <ErrorPage />,
   },
 ]);
 ReactDOM.createRoot(document.getElementById('root')).render(
