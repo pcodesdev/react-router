f6cd1b09d51f7afcd352bf1f8c17c0eb4b369154
Peter Njuguna <peter.njuguna@kirkonulkomaanapu.fi>
2023-02-08 13:44:53 +0300

    implemented client side routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,22 @@
-import React from 'react'
+import React, { Children } from 'react'
 import ReactDOM from 'react-dom/client'
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
 import './index.css'
-import Root from './routes/root';
+import Root, { loader as rootLoader } from './routes/root';
+import Contact from './routes/contact';
 import ErrorPage from './error-page';
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
+    children: [{
+      path: "contacts/:contactId",
+      element: <Contact />,
+    },],
   },
 ]);
 ReactDOM.createRoot(document.getElementById('root')).render(
