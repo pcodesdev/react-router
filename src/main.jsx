aed8df838d6d38067b04b95b5e526c4a3f689eb8
Peter Njuguna <peter.njuguna@kirkonulkomaanapu.fi>
2023-02-09 14:14:29 +0300

    add react router form

--- src/main.jsx
+++ src/main.jsx
@@ -5,18 +5,23 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import './index.css'
-import Root, { loader as rootLoader } from './routes/root';
-import Contact from './routes/contact';
+import Root, { loader as rootLoader, action as rootAction } from "./routes/root";
+import Contact, { loader as contactLoader } from './routes/contact';
 import ErrorPage from './error-page';
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
-    children: [{
-      path: "contacts/:contactId",
-      element: <Contact />,
-    },],
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+        loader: contactLoader,
+      },
+    ],
   },
 ]);
 ReactDOM.createRoot(document.getElementById('root')).render(
--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,11 +1,18 @@
-import { Outlet, Link, useLoaderData } from 'react-router-dom';
-import { getContacts } from './../contacts'
+import { Outlet, Link, useLoaderData, Form } from 'react-router-dom';
+import { getContacts, createContact } from './../contacts'
 import logo from './../assets/logo.png';
+
 
 export async function loader() {
     const contacts = await getContacts();
-    return [contacts]
+    return { contacts };
 }
+export async function action() {
+    const contacts = await createContact();
+    return { contacts };
+}
+
+
 
 export default function Root() {
     const { contacts } = useLoaderData()
@@ -35,9 +42,9 @@ export default function Root() {
                             aria-live="polite"
                         ></div>
                     </form>
-                    <form method="post">
+                    <Form method="post">
                         <button type="submit">New</button>
-                    </form>
+                    </Form>
                 </div>
                 <nav>
                     {contacts.length ? (
